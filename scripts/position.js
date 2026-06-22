64d301fc062afc0c2f7d4855d503ee836128bcd1
Brandon Fitzpatrick <113076498+BrandonFitzpatricc@users.noreply.github.com>
2026-06-22 10:10:04 -0400

    Write position module

---
+++ scripts/position.js
@@ -0,0 +1,17 @@
+class Position {
+  #value;
+  #path;
+
+  constructor(value, path) {
+    this.#value = value;
+    this.#path = path;
+  }
+
+  get value() {
+    return this.#value;
+  }
+
+  get path() {
+    return this.#path;
+  }
+}
